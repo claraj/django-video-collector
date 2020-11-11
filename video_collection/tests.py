33d0e5f3ce04f19d56327f0b1b5cf80cfbb53324
clara <10088152+claraj@users.noreply.github.com>
2020-11-11 17:25:47 -0600

    tests

--- video_collection/tests.py
+++ video_collection/tests.py
@@ -1,3 +1,233 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError, transaction
 
-# Create your tests here.
+from .models import Video
+
+
+class TestVideoList(TestCase):
+
+    # All videos shown on list page, sorted by name, case insensitive
+
+    def test_all_videos_displayed_in_correct_order(self):
+
+        v1 = Video.objects.create(name='XYZ', notes='example', url='https://www.youtube.com/watch?v=123')
+        v2 = Video.objects.create(name='ABC', notes='example', url='https://www.youtube.com/watch?v=456')
+        v3 = Video.objects.create(name='uvw', notes='example', url='https://www.youtube.com/watch?v=789')
+        v4 = Video.objects.create(name='def', notes='example', url='https://www.youtube.com/watch?v=101')
+
+        expected_video_order = [v2, v4, v3, v1]
+        response = self.client.get(reverse('video_list'))
+        videos_in_template = list(response.context['videos'])
+        self.assertEqual(expected_video_order, videos_in_template)
+
+    # No video message 
+
+    def test_no_video_message(self):
+        response = self.client.get(reverse('video_list'))
+        videos_in_template = response.context['videos']
+        self.assertContains(response, 'No videos.')
+
+
+    # 1 video vs 3 videos message
+
+    def test_video_number_message_single_video(self):
+        v1 = Video.objects.create(name='XYZ', notes='example', url='https://www.youtube.com/watch?v=123')
+        response = self.client.get(reverse('video_list'))
+        self.assertContains(response, '1 video')
+        self.assertNotContains(response, '1 videos')   # check this, because '1 videos' contains '1 video'
+
+
+    def test_video_number_message_multiple_videos(self):
+        v1 = Video.objects.create(name='XYZ', notes='example', url='https://www.youtube.com/watch?v=123')
+        v2 = Video.objects.create(name='ABC', notes='example', url='https://www.youtube.com/watch?v=456')
+        v3 = Video.objects.create(name='uvw', notes='example', url='https://www.youtube.com/watch?v=789')
+        v4 = Video.objects.create(name='def', notes='example', url='https://www.youtube.com/watch?v=101')
+
+        response = self.client.get(reverse('video_list'))
+        self.assertContains(response, '4 videos')
+
+
+    # search only shows matching videos, partial case-insensitive matches
+
+    def test_video_search_matches(self):
+        v1 = Video.objects.create(name='ABC', notes='example', url='https://www.youtube.com/watch?v=456')
+        v2 = Video.objects.create(name='nope', notes='example', url='https://www.youtube.com/watch?v=789')
+        v3 = Video.objects.create(name='abc', notes='example', url='https://www.youtube.com/watch?v=123')
+        v4 = Video.objects.create(name='hello aBc!!!', notes='example', url='https://www.youtube.com/watch?v=101')
+        
+        expected_video_order = [v1, v3, v4]
+        response = self.client.get(reverse('video_list') + '?search_term=abc')
+        videos_in_template = list(response.context['videos'])
+        self.assertEqual(expected_video_order, videos_in_template)
+
+
+    def test_video_search_no_matches(self):
+        v1 = Video.objects.create(name='ABC', notes='example', url='https://www.youtube.com/watch?v=456')
+        v2 = Video.objects.create(name='nope', notes='example', url='https://www.youtube.com/watch?v=789')
+        v3 = Video.objects.create(name='abc', notes='example', url='https://www.youtube.com/watch?v=123')
+        v4 = Video.objects.create(name='hello aBc!!!', notes='example', url='https://www.youtube.com/watch?v=101')
+        
+        expected_video_order = []  # empty list 
+        response = self.client.get(reverse('video_list') + '?search_term=kittens')
+        videos_in_template = list(response.context['videos'])
+        self.assertEqual(expected_video_order, videos_in_template)
+        self.assertContains(response, 'No videos')
+
+
+class TestAddVideos(TestCase):
+
+    # Adding a video, added to DB and video_id created 
+
+    def test_add_video(self):
+
+        valid_video = {
+            'name': 'yoga',
+            'url': 'https://www.youtube.com/watch?v=4vTJHUDB5ak',
+            'notes': 'for neck and shoulders'
+        }
+        
+        response = self.client.post(reverse('add_video'), data=valid_video, follow=True)
+
+        # redirect to video list 
+        self.assertTemplateUsed('video_collection/video_list.html')
+
+        # video list shows new video 
+        self.assertContains(response, 'yoga')
+        self.assertContains(response, 'https://www.youtube.com/watch?v=4vTJHUDB5ak')
+        self.assertContains(response, 'for neck and shoulders')
+
+        # one new video in the database 
+        video_count = Video.objects.count()
+        self.assertEqual(1, video_count)
+
+        # with the correct data, including the ID
+        valid_video['video_id'] = '4vTJHUDB5ak'
+        new_video = Video.objects.get(**valid_video)
+        self.assertIsNotNone(new_video)
+
+
+    # Invalid videos not added
+    #    Duplicates not allowed
+
+    def test_add_duplicate_video_not_added(self):
+
+        # Since an integrity error is raised, the database has to be rolled back to the state before this 
+        # action (here, adding a duplicate video) was attempted. This is a separate transaction so the 
+        # database might be in a weird state and future queries in this method can fail with atomic transaction errors. 
+        # the solution is to ensure the entire transation is in an atomic block so the attempted save and subsequent 
+        # rollback are completely finished before more DB transactions - like the count query - are attempted.
+
+        # Most of this is handled automatically in a view function, but we have to deal with it here. 
+
+        with transaction.atomic():
+            new_video = {
+                'name': 'yoga',
+                'url': 'https://www.youtube.com/watch?v=4vTJHUDB5ak',
+                'notes': 'for neck and shoulders'
+            }
+
+            # Create a video with this data in the database
+            # the ** syntax unpacks the dictonary and converts it into function arguments
+            # https://python-reference.readthedocs.io/en/latest/docs/operators/dict_unpack.html
+            # Video.objects.create(**new_video)
+            # with the new_video dictionary above is equivalent to 
+            # Video.objects.create(name='yoga', url='https://www.youtube.com/watch?v=4vTJHUDB5ak', notes='for neck and shoulders')
+            Video.objects.create(**new_video)
+
+            video_count = Video.objects.count()
+            self.assertEqual(1, video_count)
+
+        with transaction.atomic():
+            # try to create it again    
+            response = self.client.post(reverse('add_video'), data=new_video, follow=True)
+
+            # same template, the add form 
+            self.assertTemplateUsed('video_collection/add.html')
+
+            messages = response.context['messages']
+            message_texts = [ message.message for message in messages ]
+            self.assertIn('You already added that video', message_texts)
+
+        # still one video in the database 
+        video_count = Video.objects.count()
+        self.assertEqual(1, video_count)
+
+    def test_add_video_invalid_url_not_added(self):
+
+        invalid_video_urls = [
+            'https://www.youtube.com/watch',
+            'https://www.youtube.com/watch?',
+            'https://www.youtube.com/watch?abc=sdsdfs',
+            'https://www.youtube.com/watch?v',
+            'https://www.github.com/',
+            'https://www.github.com?v=12345',
+        ]
+
+        for invalid_url in invalid_video_urls:
+
+            new_video = {
+                'name': 'yoga',
+                'url': invalid_url,
+                'notes': 'for neck and shoulders'
+            }
+
+            response = self.client.post(reverse('add_video'), data=new_video, follow=True)
+
+            # same template, the add form 
+            self.assertTemplateUsed('video_collection/add.html')
+
+            messages = response.context['messages']
+            message_texts = [ message.message for message in messages ]
+            self.assertIn('Invalid YouTube URL', message_texts)
+
+            # no videos in the database 
+            video_count = Video.objects.count()
+            self.assertEqual(0, video_count)
+
+
+class TestVideoModel(TestCase):
+
+    def test_create_id(self):
+        video = Video.objects.create(name='example', url='https://www.youtube.com/watch?v=IODxDxX7oi4')
+        self.assertEqual('IODxDxX7oi4', video.video_id)
+
+
+    def test_create_id_valid_url_with_time_parameter(self):
+        # a video that is playing and paused may have a timestamp in the query
+        video = Video.objects.create(name='example', url='https://www.youtube.com/watch?v=IODxDxX7oi4&ts=14')
+        self.assertEqual('IODxDxX7oi4', video.video_id)
+
+
+    def test_invalid_urls_raise_validation_error(self):
+        invalid_video_urls = [
+            'https://www.youtube.com/watch',
+            'https://www.youtube.com/watch?',
+            'https://www.youtube.com/watch?abc=sdsdfs',
+            'https://www.youtube.com/watch?v',
+            'https://www.github.com/',
+            'https://www.github.com?v=12345',
+        ]
+
+        for invalid_url in invalid_video_urls:
+            with self.assertRaises(ValidationError):
+                new_video = {
+                    'name': 'example',
+                    'url': invalid_url,
+                    'notes': 'example note'
+                }
+
+                # Create a video with this data in the database
+                Video.objects.create(**new_video)
+
+        video_count = Video.objects.count()
+        self.assertEqual(0, video_count)
+
+
+    def duplicate_video_raises_integrity_error(self):
+
+        Video.objects.create(name='example', url='https://www.youtube.com/watch?v=IODxDxX7oi4')
+        with self.assertRaises(IntegrityError):
+            Video.objects.create(name='example', url='https://www.youtube.com/watch?v=IODxDxX7oi4')
+        
